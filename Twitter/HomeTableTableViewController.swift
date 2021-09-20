596e82204caedbdcf75886b6734eb2c5500b3c35
izlatkin <zlatkin_ilya@mail.ru>
2021-09-19 20:18:47 -0400

    User stays logged in across restarts

--- Twitter/HomeTableTableViewController.swift
+++ Twitter/HomeTableTableViewController.swift
@@ -13,6 +13,7 @@ class HomeTableTableViewController: UITableViewController {
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
+        UserDefaults.standard.set(false,forKey: "userLoggedIn")
     }
     
     override func viewDidLoad() {
--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -14,11 +14,18 @@ class LoginViewController: UIViewController {
         //print("Hellow world")
         let myURL = "https://api.twitter.com/oauth/request_token"
         TwitterAPICaller.client?.login(url: myURL, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "LoginToHome", sender: self)
         }, failure: { Error in
             print("Code not login")
         })
         
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "LoginToHome", sender: self)
+        }
     }
     
     override func viewDidLoad() {
